d6f7d6cc931bdb447d3b1504f2ad93e3dae252c9
brownguo <yexuan.guo@gmail.com>
2021-10-12 15:43:33 +0800

    sort

---
+++ quick_sort.py
@@ -0,0 +1,12 @@
+
+class quickSort(object):
+    def __init__(self):
+        pass
+
+    def quick_sort(self, arr):
+        print(arr)
+
+
+if __name__ == '__main__':
+    start = quickSort()
+    start.quick_sort([3, 6, 9, 2, 4, 1])
